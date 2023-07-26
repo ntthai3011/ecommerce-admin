e33525abdd9f4a08f854f8b703ee3844eb49c61e
ntthai3011 <ntthai@whitespace.vn>
2023-07-26 22:08:23 +0700

    initial project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='p-5'>
+        <Button size="default" variant="destructive">Click me</Button>
+    </div>
+  )
+}
